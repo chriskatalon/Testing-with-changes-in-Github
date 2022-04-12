abc599674cd28cd1ab272a05f22da695b524e075
quan.trinh <quan.trinh@GF0X2D3>
2022-04-12 17:16:16 +0700

    orginal file

---
+++ Test Suites/Go to gg.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Go to gg</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>94d15b72-69c5-4137-8477-399f1e4d8c64</testSuiteGuid>
+</TestSuiteEntity>
