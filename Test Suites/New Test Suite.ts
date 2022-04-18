1099f216523e1148df29db5570a3a34ff35295b8
quan.trinh <quan.trinh@GF0X2D3>
2022-04-18 14:53:59 +0700

    a

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>054bf0e8-81f3-4a42-9c6b-318725cc27ca</testSuiteGuid>
+   <testCaseLink>
+      <guid>d2c00d0b-4bc1-49ff-919f-0b7caf5226cd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Go to katalon</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
